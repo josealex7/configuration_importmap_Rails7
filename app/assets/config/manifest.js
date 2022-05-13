accbd6e25ed25aedcba4ef15399035d3285a3c93
alex_tavera <alexander.tavera@iungoeducation.com>
2022-05-13 12:56:41 -0500

    Configuracion incial de importmaps, crear funcion de prueba y compilar los assets

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= link_tree ../images
+//= link_tree ../builds
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
+//= link custom/main.js
---
+++ app/javascript/custom/main.js
@@ -0,0 +1,12 @@
+function uniToggleShow() {
+    //document.querySelectorAll(".uni-toggle").forEach(e => e.classList.remove("hidden"))
+    console.log('yo ya estoy 1')
+}
+
+function uniToggleHide() {
+    console.log("yo ya estoy 2")
+    //document.querySelectorAll(".uni-toggle").forEach(e => e.classList.add("hidden"))
+}
+
+window.uniToggleShow = uniToggleShow
+window.uniToggleHide = uniToggleHide
